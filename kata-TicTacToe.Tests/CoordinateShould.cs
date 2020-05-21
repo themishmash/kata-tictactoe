fff677b69e3237dfc9418c2b21fd2e4167b04bb5
Michelle <wong.mich.c@gmail.com>
2020-05-21 14:54:31 +1000

    created coordinate test class

---
+++ kata-TicTacToe.Tests/CoordinateShould.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace kata_TicTacToe.Tests
+{
+    public class CoordinateShould
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
