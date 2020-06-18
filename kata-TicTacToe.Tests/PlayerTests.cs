061f9d80e094c5479fb3137fcb460af1b98d1d4e
Michelle <wong.mich.c@gmail.com>
2020-06-18 22:00:44 +1000

    player tests pass with name property. Need to do same for tic tac toe class

--- kata-TicTacToe.Tests/PlayerTests.cs
+++ kata-TicTacToe.Tests/PlayerTests.cs
@@ -2,30 +2,30 @@ using Xunit;
 
 namespace kata_TicTacToe.Tests
 {
-    // public class PlayerTests
-    // {
-    //     
-    //     [Fact]
-    //      public void PlaceSymbolWithXAndYCoordinates()
-    //      {
-    //          //arrange
-    //          var input = new PlayerInput((1,1));
-    //          var playerX = new Player(input, Symbol.Cross);
-    //          //act
-    //          var move = playerX.PlayTurn();
-    //          //assert
-    //          Assert.Equal(1, move.XCoordinate);
-    //          Assert.Equal(1, move.YCoordinate);
-    //      }
-    //     
-    //      
-    //     [Fact]
-    //     public void PlayerHasCorrectXSymbol()
-    //     {
-    //         var input = new PlayerInput((1,1));
-    //         var playerX = new Player(input, Symbol.Cross);
-    //         Assert.Equal(Symbol.Cross, playerX.Symbol);
-    //     }
-    //     
-    // }
+    public class PlayerTests
+    {
+        
+        [Fact]
+         public void PlaceSymbolWithXAndYCoordinates()
+         {
+             //arrange
+             var input = new PlayerInput((1,1));
+             var playerX = new Player(input, Symbol.Cross, "player X");
+             //act
+             var move = playerX.PlayTurn();
+             //assert
+             Assert.Equal(1, move.XCoordinate);
+             Assert.Equal(1, move.YCoordinate);
+         }
+        
+         
+        [Fact]
+        public void PlayerHasCorrectXSymbol()
+        {
+            var input = new PlayerInput((1,1));
+            var playerX = new Player(input, Symbol.Cross, "player X");
+            Assert.Equal(Symbol.Cross, playerX.Symbol);
+        }
+        
+    }
 }
--- kata-TicTacToe/Program.cs
+++ kata-TicTacToe/Program.cs
@@ -11,8 +11,7 @@ namespace kata_TicTacToe
             //ask question of name here
             Console.WriteLine("Player 1: what is your name?");
             var name = Console.ReadLine();
-            var player1 = new Player(consoleInputOutput, Symbol.Cross, name); //answer to question as part of 
-            //constructor
+            var player1 = new Player(consoleInputOutput, Symbol.Cross, name);  
             Console.WriteLine("Player 2: what is your name?");
             var name2 = Console.ReadLine();
             var player2 = new Player(consoleInputOutput, Symbol.Naught, name2);
